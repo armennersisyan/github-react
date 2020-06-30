f0bb74cfc3214e4827fed31431ec8e592a3db814
Armen Nersisyan <armenersisyan@gmail.com>
2020-06-30 20:46:02 +0400

    header is changed to functional component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,33 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import styles from './style.module.scss'
 import PropTypes from "prop-types";
 
-class Header extends PureComponent {
-  static propTypes = {
-    search: PropTypes.string.isRequired,
-    isLoading: PropTypes.bool,
-    onChange: PropTypes.any,
-  };
-  
-  render() {
-    const { search, isLoading, onChange } = this.props;
-    return (
-      <header className={styles['header']}>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4 col-sm-6">
-              <div className={styles['users-search']}>
-                <input
-                  type="text"
-                  placeholder="Search for users by login..."
-                  disabled={isLoading}
-                  value={search}
-                  onChange={onChange}
-                />
-              </div>
+function Header({ search, isLoading, onChange }) {
+  return (
+    <header className={styles['header']}>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 col-sm-6">
+            <div className={styles['users-search']}>
+              <input
+                type="text"
+                placeholder="Search for users by login..."
+                disabled={isLoading}
+                value={search}
+                onChange={onChange}
+              />
             </div>
           </div>
         </div>
-      </header>
-    );
-  }
+      </div>
+    </header>
+  );
 }
+
+Header.propTypes = {
+  search: PropTypes.string.isRequired,
+  isLoading: PropTypes.bool,
+  onChange: PropTypes.any,
+};
 
 export default Header;
